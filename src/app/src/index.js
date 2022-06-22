e975d3317fa2e6ae5a0bf5e7a20046dee5d0ef95
polycloud-dev <heidenreichdd@gmail.com>
2022-06-22 17:43:35 +0200

    added theme provider

--- src/app/src/index.js
+++ src/app/src/index.js
@@ -3,11 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './global.css';
 import App from './App/App';
 import reportWebVitals from './reportWebVitals';
+import theme from './theme.json';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <MantineProvider
+      theme={theme}
+    >
+      <App />
+    </MantineProvider>
   </React.StrictMode>
 );
 
