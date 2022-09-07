d99c97af4365eba426d739faa010f4aff722b7cc
polycloud-dev <heidenreichdd@gmail.com>
2022-09-07 10:22:44 +0200

    added data endpoints

--- backend/functions/src/index.ts
+++ backend/functions/src/index.ts
@@ -1,16 +1,30 @@
 import * as functions from "firebase-functions";
+import express from "express";
+import cors from "cors";
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+import * as products_json from "../data/products.json";
+import * as bundles_json from "../data/bundles.json";
+import * as class_bundles_json from "../data/class_bundles.json";
+
+const app = express();
+app.use(cors())
+
+// serve product data
+app.get("/products", (_, res) => {
+    res.json(products_json);
+});
+app.get("/bundles", (_, res) => {
+    res.json(bundles_json);
+});
+app.get("/class_bundles", (_, res) => {
+    res.json(class_bundles_json);
+});
+
+export const data = functions.https.onRequest(app);
 
 // /order endpoint
 export const order = functions.https.onRequest((request, response) => {
     
-    functions.logger.info("Hello logs!", { structuredData: true });
+    functions.logger.info(request.body, {structuredData: true});
     response.send("Hello from Firebase!");
 });
