c656dcbad83921100ab3e24abfb6f5314d20d7a2
polycloud-dev <heidenreichdd@gmail.com>
2022-09-08 16:46:12 +0200

    cleaned some things

--- dev-server/dev_server.js
+++ dev-server/dev_server.js
@@ -12,19 +12,6 @@ app.use(cors())
 // serve images
 app.use('/images', express.static(path.join(__dirname, '../images')));
 
-// serve files
-app.get('/data/bundles', (_, res) => {
-    res.setHeader('Content-Type', 'application/json').sendFile(path.join(__dirname, '../data/bundles.json'));
-})
-
-app.get('/data/class_bundles', (_, res) => {
-    res.setHeader('Content-Type', 'application/json').sendFile(path.join(__dirname, '../data/class_bundles.json'));
-})
-
-app.get('/data/products', (_, res) => {
-    res.setHeader('Content-Type', 'application/json').sendFile(path.join(__dirname, '../data/products.json'));
-})
-
 // listen
 app.listen(PORT, () => {
     console.log(`listening on port ${PORT}`);
