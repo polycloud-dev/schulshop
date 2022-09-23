661c52a19df74b6e35708216bc33670d9df4a920
polycloud-dev <heidenreichdd@gmail.com>
2022-09-23 14:45:24 +0200

    fixed yt link

--- src/pages/printer.js
+++ src/pages/printer.js
@@ -2,7 +2,7 @@ import { useEffect } from "react"
 
 export default function PrinterEasterEgg() {
     useEffect(() => {
-        window.location.href = 'https://www.youtube.com/watch?v=jeg_TJvkSjg&t=42s'
+        window.location.href = 'https://www.youtube.com/watch?v=jeg_TJvkSjg&t=25s'
     }, [])
     return "How did you find me??"
 }
