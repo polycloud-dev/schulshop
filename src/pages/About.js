2de6d9ba0eea695091bc1a07e1eab49fd5df0d83
polycloud-dev <heidenreichdd@gmail.com>
2022-09-23 14:45:04 +0200

    removed incorrect text

--- src/pages/About.js
+++ src/pages/About.js
@@ -23,15 +23,6 @@ export default function AboutPage() {
                 >
                     Der Erwerb aller benötigten Schulmaterialien soll der Schulgemeinschaft einfach und kostengünstig zugänglich gemacht werden.
                     Dabei wird auf Nachhaltigkeit und regionale Geschäftspartner besonders geachtet.
-                    Die Produkte sollen individuell auf die jeweiligen Klassen zugeschnitten werden.
-                    Das bedeutet, dass die Schülerinnen und Schüler bereits in den Sommerferien per Online-Shop ihre Unterlagen bestellen können,
-                    ohne zu wissen, was sie überhaupt alles benötigen.
-                    z.B. ein Schüler der jetzigen 6b weiß zwar,
-                    dass er nächstes Schuljahr in der 7b sein wird, aber nicht,
-                    welche Lehrer er bekommen wird und demnach auch nicht, welches Material er benötigen wird.
-                    Der ASG-Schulshop erhält bereits im Juli die entsprechenden Daten, und kann individuelle Klassenpakete
-                    (die auf jede Klasse zugeschnitten sind) erstellen. Diese können die Schülerinnen und Schüler schon vor Beginn des neuen Schuljahres bestellen.
-                    Am ersten bzw. zweiten Schultag werden diese Pakete dann den entsprechenden Schülerinnen und Schülern in die Klassen geliefert.
                 </Text>
                 {/* <Group
                     mt='4rem'
